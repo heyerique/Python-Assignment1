c0bd040a4105bfee4e8e895d882af4ada33ec992
Enrique <36683200+heyerique@users.noreply.github.com>
2018-03-08 14:27:25 +1300

    realise data validation for BMI and salary

--- src/data_validator.py
+++ src/data_validator.py
@@ -1,4 +1,4 @@
-# import re
+import re
 
 class DataValidator:
     @staticmethod
@@ -14,13 +14,41 @@ class DataValidator:
         return sales
 
     @staticmethod
-    def check_bmi(bmi):
-        return bmi
+    def check_bmi(input_bmi):
+        """
+        Check if the input BMI is valid.
+        :param input_bmi: user input
+        :return: Formatted BMI if the input one is valid, otherwise, return None
+        :Author: Zhiming Liu
+        """
+        bmi = str(input_bmi)
+        pattern = r".*(?P<bmi>normal|overweight|obesity|underweight).*"
+        match_obj = re.search(pattern, bmi, re.I)
+        if match_obj:
+            bmi = match_obj.group("bmi") # Find the match
+            bmi = " ".join(text[0].upper() + text[1:] for text in bmi.split()) # Capitalise the first letter
+            return bmi
+        return None
 
     @staticmethod
-    def check_salary(salary):
-        return salary
+    def check_salary(input_salary):
+        """
+        Check if the input salary is valid.
+        :param input_salary: user input
+        :return: Formatted salary if the input one is valid, otherwise, return None
+        :Author: Zhiming Liu
+        """
+        salary = input_salary
+        pattern = r"\D(?P<salary>[0-9]{2,3})\D"
+        match_obj = re.search(pattern, salary)
+        if match_obj:
+            return match_obj["salary"]
+        return None
 
     @staticmethod
     def check_birthday(birthday):
         return birthday
+
+
+# print(DataValidator.check_bmi("jbjndsoidiri88888normaljdjdjd"))
+# print(DataValidator.check_salary("$001TT TW"))
