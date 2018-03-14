b2dacd1f9f7672dfc1b94b448bc4edc9a8985890
Enrique <36683200+heyerique@users.noreply.github.com>
2018-03-14 14:43:14 +1300

    remove from wrong place

--- data_console.py
+++
@@ -1,8 +0,0 @@
-class DataConsole:
-    data = []
-    _data_sources = "csv", "database", "web"
-    _source = None
-
-    def select_source(self, source):
-        if source in self._data_sources:
-            self._source =
